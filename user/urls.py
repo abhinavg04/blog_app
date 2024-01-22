32ce91cc0637b35a86be40e3be9e0af4c482ad11
abhinavg04 <abhinavg1024@gmail.com>
2024-01-22 18:57:44 +0530

    word count

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from user.views import *
 urlpatterns = [
     path('',DashBoardView.as_view(),name="dash"),
-    path('add',AdditionView.as_view(),name="add")
+    path('add',AdditionView.as_view(),name="add"),
+    path('mul',MulView.as_view(),name="mul"),
+    path('word',WordCount.as_view(),name="word"),
 ]
--- user/views.py
+++ user/views.py
@@ -22,3 +22,31 @@ class AdditionView(View):
         context={}
         context["data"] = sum
         return render(request,'addition.html',context)
+class MulView(View):
+    def get(self,request):
+        return render(request,"addition.html")
+    def post(self,request):
+        num1 = request.POST.get('num1')
+        num2 = request.POST.get('num2')
+        try:
+            sum = int(num1)*int(num2)
+        except:
+            sum = "error"
+        context={}
+        context["data"] = sum
+        return render(request,'multiplication.html',context)
+class WordCount(View):
+    def get(self,request):
+        return render(request,"word_count.html")
+    def post(self,request):
+        sentence = request.POST.get('text')
+        word_list = sentence.split(" ")
+        d = {}
+        for word in word_list:
+            if word in d:
+                d[word] += 1
+            else:
+                d[word] = 1
+        context={}
+        context["data"] = d
+        return render(request,'word_count.html',context)
